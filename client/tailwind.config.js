55aa1244f915680fcc4f81969ae750349c1cad89
Timothy <mbacs@Timothys-MacBook-Air.local>
2023-01-22 02:34:53 -0800

    updated color in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#0F172A",
+        heading: "#F8FAFC",
+        message: "#8E9CB1",
+        darkPurple: "#4338CA",
+        lightPurple: "#6366F1",
+        inactive: "#334155"
+
+      }
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      fira: ["Fira Code", "monospace"]
+    },
+
   },
   plugins: [],
 }
