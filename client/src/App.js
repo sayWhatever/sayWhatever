83b888280f1d4cd0a3f68088a0166a9ce7cb5e3c
Dylan Sung <dys907@gmail.com>
2023-01-21 16:46:13 -0800

    dummy socket code for connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
 import './App.css';
 import Chatbox from './components/Chatbox';
 import { io } from 'socket.io-client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-useEffect 
+const socket = io("localhost:5000/", {
+  transports: ["websocket"]
+});
 
-function App() {
+const App = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  useEffect(() => {
+    console.log("useEffect run");
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('pong', () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit('ping');
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2> Say Whatever</h2>
-
-        <Chatbox/>
-
-      </header>
+    <div>
+      <h2> Say Whatever</h2>
+      <Chatbox callMethod={sendPing}/>
+      <p>Connected: {'' + isConnected}</p>
+      <p>Last pong: {lastPong || '-'}</p>
+      <button onClick={sendPing}>Send ping</button>
     </div>
   );
 }
+
+
 
 export default App;
