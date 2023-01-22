5334f2da7c5b9b6319c271864ce5dbf3f54dac53
Dylan Sung <dys907@gmail.com>
2023-01-21 21:34:21 -0800

    delete message on send to server

--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
 
-const Chatbox = ({userName, socket, messages}) => {
+const Chatbox = ({ userName, socket, messages }) => {
 
-    const [message,setMessage] = useState("");
+    const [message, setMessage] = useState("");
 
     const handleMessage = (event) => {
         setMessage(event.target.value);
@@ -13,26 +13,26 @@ const Chatbox = ({userName, socket, messages}) => {
     const sendMessage = clientMessage => {
         console.log("message", userName + " : " + clientMessage)
         socket.emit("msgToServer", userName + " : " + clientMessage);
+        setMessage("");
     }
-
 
     return (
         <>
-        <div>
-            {
-                messages.map((message, i)=>
-                    <p key={i}>{message}</p>
-                )
-            }
-        </div>
-        <input
+            <div>
+                {
+                    messages.map((message, i) =>
+                        <p key={i}>{message}</p>
+                    )
+                }
+            </div>
+            <input
                 type="text"
                 id="message"
                 name="message"
                 onChange={handleMessage}
                 value={message}
             />
-            <button onClick={()=>{sendMessage(message)}} ><i>send</i></button>
+            <button onClick={() => { sendMessage(message) }} ><i>send</i></button>
         </>
     )
 }
