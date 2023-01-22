6460f3ced7abb6e2742e3af9e6236961ce9a1292
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-01-21 22:03:07 -0800

    .

--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -17,12 +17,12 @@ const Chatbox = ({userName, socket}) => {
     return (
         <section class="flex flex-col items-center">
             <div class="">
-                <div class="bg-white h-60 rounded-t-lg p-4 shadow-md">
+                <div class="bg-inactive h-60 rounded-t-lg p-4 shadow-md">
                     <p>Messages would go here</p>
                 </div>
-                <div class="bg-white rounded-b-lg shadow-md">
+                <div class="bg-inactive rounded-b-lg shadow-md">
                     <input  
-                        class="text-message focus:outline-none rounded-xl border-2 px-2 w-64 h-10 bg-background"
+                        class="focus:shadow-outline text-message focus:outline-none rounded-xl border-2 px-2 h-10 bg-background w-full"
                         placeholder="enter your message here"
                                 type="text"
                                 id="message"
@@ -30,7 +30,7 @@ const Chatbox = ({userName, socket}) => {
                                 onChange={handleMessage}
                                 value={message}
                         />
-                    <button onClick={()=>{sendMessage(message)}} >send</button>
+                    <button class="w-10 bg-inactive rounded-lg p-auto text-heading" onClick={()=>{sendMessage(message)}}>send</button>
                 </div>
             </div>
         </section>
